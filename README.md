0c959c1aa1aa5d8b5be994e0ad1aa4e7e79e501c
Mitch Anderson <mitch.anderson@undeadops.xyz>
2022-04-17 19:54:28 -0600

    feat(subnets): Adding Public subnets, igw, and route table

--- README.md
+++ README.md
@@ -15,8 +15,20 @@ pulumi config set name dev # Matches stack name
 pulumi config set --path 'vpc:cidrblock' 10.10.0.0/16
 pulumi config set --path 'vpc:dnshostnames' true
 pulumi config set --path 'vpc:dnssupport' true
+pulumi config set --path 'vpc.azs[0]' "us-east-2a"
+pulumi config set --path 'vpc.azs[1]' "us-east-2b"
+pulumi config set --path 'vpc.azs[2]' "us-east-2c"
+pulumi config set --path 'vpc.publicsubnets[0]' '10.20.0.0/24'
+pulumi config set --path 'vpc.publicsubnets[1]' '10.20.1.0/24'
+pulumi config set --path 'vpc.publicsubnets[2]' '10.20.2.0/24'
+pulumi config set --path 'vpc.privatesubnets[0]' '10.20.32.0/19'
+pulumi config set --path 'vpc.privatesubnets[1]' '10.20.64.0/19'
+pulumi config set --path 'vpc.privatesubnets[2]' '10.20.96.0/19'
+pulumi config set --path 'vpc.dbsubnets[0]' '10.20.20.0/24'
+pulumi config set --path 'vpc.dbsubnets[1]' '10.20.22.0/24'
+pulumi config set --path 'vpc.dbsubnets[2]' '10.20.24.0/24'
 pulumi config set --path 'vpc:tags[0]' 'costcenter:platform'
-pulumi config set --path 'vpc:tags[1]' 'env:dev' # matches stack name
+pulumi config set --path 'vpc:singlenatgateway' true # false creates one nat gateway per private subnet
 ```
 
 As long as your AWS credentials are setup and working.
--- vpc.go
+++ vpc.go
@@ -1,6 +1,7 @@
 package awsvpc
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
@@ -8,11 +9,15 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+// VpcOpts - Very much a work in progress
 type VpcOpts struct {
-	CidrBlock    string
-	DnsHostnames bool
-	DnsSupport   bool
-	Tags         []string
+	Name             string
+	Cidr             string
+	Azs              []string
+	Publicsubnets    []string
+	Privatesubnets   []string
+	Singlenatgateway bool
+	Tags             []string
 }
 
 func Vpc(ctx *pulumi.Context) error {
@@ -22,27 +27,95 @@ func Vpc(ctx *pulumi.Context) error {
 	name := cfg.Require("name")
 	cfg.RequireObject("vpc", &opts)
 
-	// Render Tags
-	tags := pulumi.StringMap{}
-	for _, l := range opts.Tags {
-		t := strings.Split(l, ":")
-		tags[strings.TrimSpace(t[0])] = pulumi.String(strings.TrimSpace(t[1]))
-	}
-
-	// Set/Override Name Tag
-	tags["Name"] = pulumi.String(name)
+	// eh...
+	opts.Name = name
 
 	// Build VPC
 	vpc, err := ec2.NewVpc(ctx, name, &ec2.VpcArgs{
-		CidrBlock:          pulumi.String(opts.CidrBlock),
-		EnableDnsHostnames: pulumi.Bool(opts.DnsHostnames),
-		EnableDnsSupport:   pulumi.Bool(opts.DnsSupport),
-		Tags:               tags,
+		CidrBlock:          pulumi.String(opts.Cidr),
+		EnableDnsHostnames: pulumi.Bool(true),
+		EnableDnsSupport:   pulumi.Bool(true),
+		Tags:               pulumiTags(name, opts.Tags),
+	})
+	if err != nil {
+		return err
+	}
+
+	// Internet Gateway
+	igw, err := ec2.NewInternetGateway(ctx, name+"-igw", &ec2.InternetGatewayArgs{
+		VpcId: vpc.ID(),
+		Tags:  pulumiTags(name+"-igw", opts.Tags),
+	})
+	if err != nil {
+		return err
+	}
+
+	// Route Table
+	_, err = ec2.NewRouteTable(ctx, name+"-rt", &ec2.RouteTableArgs{
+		Routes: ec2.RouteTableRouteArray{
+			ec2.RouteTableRouteArgs{
+				CidrBlock: pulumi.String("0.0.0.0/0"),
+				GatewayId: igw.ID(),
+			},
+		},
+		VpcId: vpc.ID(),
+		Tags:  pulumiTags(name+"-rt", opts.Tags),
 	})
 	if err != nil {
 		return err
 	}
 
 	ctx.Export("vpc", vpc)
+
+	_, err = opts.publicsubnets(ctx, vpc)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (opts *VpcOpts) publicsubnets(ctx *pulumi.Context, vpc *ec2.Vpc) ([]*ec2.Subnet, error) {
+	var subnets []*ec2.Subnet
+	//var exportCidrs []string
+	//var exportSubnetIds []string
+	for k, s := range opts.Publicsubnets {
+		name := fmt.Sprintf("%s-public-subnet-%s", opts.Name, opts.Azs[k])
+		tags := pulumiTags(name, opts.Tags)
+		subnet, err := ec2.NewSubnet(ctx, name, &ec2.SubnetArgs{
+			VpcId:               vpc.ID(),
+			CidrBlock:           pulumi.String(s),
+			AvailabilityZone:    pulumi.String(opts.Azs[k]),
+			MapPublicIpOnLaunch: pulumi.Bool(true),
+			Tags:                tags,
+		}, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		// Append Subnets
+		subnets = append(subnets, subnet)
+		//exportCidrs = append(exportCidrs, s)
+		//exportSubnetIds = append(exportSubnetIds, subnet.Id)
+	}
+
+	// Export Subnets
+	//ctx.Export("publicSubnetCidrs", exportCidrs)
+	//ctx.Export("publicSubnetIds", exportSubnetIds)
+	return subnets, nil
+}
+
+func pulumiTags(name string, tags []string) pulumi.StringMap {
+	retTags := pulumi.StringMap{}
+	t := make(map[string]string)
+	for _, l := range tags {
+		x := strings.Split(l, ":")
+		t[strings.TrimSpace(x[0])] = strings.TrimSpace(x[1])
+	}
+	for k, v := range t {
+		retTags[k] = pulumi.String(v)
+	}
+
+	retTags["Name"] = pulumi.String(name)
+	return retTags
 }
